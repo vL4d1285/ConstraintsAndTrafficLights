99a649f377eceeafdeef7669eea5ced1148001ab
Vladislav Ryabtsev <vryabtsev@me.com>
2021-12-21 10:34:29 +0300

    created trafficLight and constraints task

---
+++ ConstraintsAndTrafficLights/ConstraintsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ConstraintsViewController.swift
+//  ConstraintsAndTrafficLights
+//
+//  Created by Vlad Ryabtsev on 20.12.2021.
+//
+
+import UIKit
+
+class ConstraintsViewController: UIViewController {
+
+}
+
---
+++ ConstraintsAndTrafficLights/TrafficLightsViewController.swift
@@ -0,0 +1,82 @@
+//
+//  TrafficLightsViewController.swift
+//  ConstraintsAndTrafficLights
+//
+//  Created by Vlad Ryabtsev on 20.12.2021.
+//
+
+import UIKit
+
+enum Colors {
+    case red, orange, green
+}
+
+class TrafficLightsViewController: UIViewController {
+
+    @IBOutlet var redLightOutlet: UIView!
+    @IBOutlet var orangeLightOutlet: UIView!
+    @IBOutlet var greenLightOutlet: UIView!
+              
+    @IBOutlet var startButtonOutlet: UIButton!
+    
+    var currentColor: Colors = .red
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        redLightOutlet.alpha = 0.2
+        orangeLightOutlet.alpha = 0.2
+        greenLightOutlet.alpha = 0.2
+        
+        startButtonOutlet.setTitle("START", for: .normal)
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+                       
+        redLightOutlet.layer.cornerRadius = redLightOutlet.layer.frame.height / 2
+        orangeLightOutlet.layer.cornerRadius = orangeLightOutlet.layer.frame.height / 2
+        greenLightOutlet.layer.cornerRadius = greenLightOutlet.layer.frame.height / 2
+    }
+      
+
+    @IBAction func startButtonAction() {
+        startButtonOutlet.setTitle("NEXT", for: .normal)
+        
+        if currentColor == .red {
+            redLightOutlet.alpha = 1
+            orangeLightOutlet.alpha = 0.2
+            greenLightOutlet.alpha = 0.2
+            
+            currentColor = .orange
+        } else if currentColor == .orange {
+            redLightOutlet.alpha = 0.2
+            orangeLightOutlet.alpha = 1
+            greenLightOutlet.alpha = 0.2
+            
+            currentColor = .green
+        } else {
+            redLightOutlet.alpha = 0.2
+            orangeLightOutlet.alpha = 0.2
+            greenLightOutlet.alpha = 1
+        
+            currentColor = .red
+        }
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+
+}
